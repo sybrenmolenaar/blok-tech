c95b7135b6053abdacd8942a70033f1918859b41
Sybren Molenaar <55452337+sybrenmolenaar@users.noreply.github.com>
2022-03-01 17:13:07 +0100

    added new data, restaurant.

--- server.js
+++ server.js
@@ -1,6 +1,71 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000;
+const movies = [
+  {
+          "id": 1,
+          "slug": "Instock",
+          "name": "Instock",
+          "facilities": ["wheelchair friendly stairs", "lift"],
+          "Reviews": "Good service, lovely people. Five stars",
+        },
+        {
+          "id": 2,
+          "slug": "waterkant",
+          "name": "Waterkant",
+          "facilities": ["wheelchair friendly stairs", "lift"],
+          "Reviews": "Good service, lovely people. Five stars",
+        },
+        {
+          "id": 3,
+          "slug": "mrandmrswatson",
+          "name": "Mr & Mrs Watson",
+          "facilities": ["wheelchair friendly stairs", "lift"],
+          "Reviews": "Good service, lovely people. Five stars",
+        },
+        {
+          "id": 4,
+          "slug": "volkshotel",
+          "name": "Volkhotel",
+          "facilities": ["wheelchair friendly stairs", "lift"],
+          "Reviews": "Good service, lovely people. Five stars",
+        },
+        {
+          "id": 5,
+          "slug": "tapastheater",
+          "name": "Tapas Theater",
+          "facilities": ["wheelchair friendly stairs", "lift"],
+          "Reviews": "Good service, lovely people. Five stars",
+        },
+        {
+          "id": 6,
+          "slug": "tolhuistuin",
+          "name": "Tolhuistuin",
+          "facilities": ["wheelchair friendly stairs", "lift"],
+          "Reviews": "Good service, lovely people. Five stars",
+        },
+        {
+          "id": 7,
+          "slug": "loetje",
+          "name": "Loetje",
+          "facilities": ["wheelchair friendly stairs", "lift"],
+          "Reviews": "Good service, lovely people. Five stars",
+        },
+{
+          "id": 8,
+          "slug": "tolhuistuin",
+          "name": "Tolhuistuin",
+          "facilities": ["wheelchair friendly stairs", "lift"],
+          "Reviews": "Good service, lovely people. Five stars",
+        },
+        {
+          "id": 3,
+          "slug": "febo",
+          "name": "FEBO",
+          "facilities": ["wheelchair friendly stairs", "lift"],
+          "Reviews": "Good service, lovely people. Five stars",
+        },
+];
 
 app.get('/harriet', (req, res) => {
   res.send('Hello Harriet!!')  
