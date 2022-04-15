be7356cdfab52c7eecfb75af5ddfd34f495a1cc7
Sybren Molenaar <55452337+sybrenmolenaar@users.noreply.github.com>
2022-04-15 12:01:04 +0200

    tried to match the data to the form but it didn't work

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ import {
 } from "mongodb";
 dotenv.config();
 
-
 let db = null;
 console.log(process.env.TESTVAR);
 const haltes = [{
@@ -113,7 +112,8 @@ app.get('/', (req, res) => {
     title
   });
 
-})
+
+});
 
 app.get('/restaurants/:id/:slug', (req, res) => {
   // FIND RESTAURANT
@@ -125,7 +125,7 @@ app.get('/restaurants/:id/:slug', (req, res) => {
     title: `details for restaurant ${restaurant.name}`,
     restaurant
   });
-})
+});
 
 app.get('/restaurant/add', (req, res) => {
   // RENDER PAGE
@@ -133,22 +133,22 @@ app.get('/restaurant/add', (req, res) => {
   res.render('addrestaurant', {
     title
   });
-})
+});
 
-app.post('/ditistijdelijk', (req, res) => {
+// app.post('/ditistijdelijk', (req, res) => {
 
-  const restaurant = {
-    name: req.body.name
-  }
-  halte.push(halte);
-  console.log("this", haltes);
-  title = "Het is gelukt!";
-  res.render('restaurantlist', {
-    title,
-    haltes
-  });
+//   const restaurant = {
+//     name: req.body.name
+//   }
+//   halte.push(halte);
+//   console.log("this", haltes);
+//   title = "Het is gelukt!";
+//   res.render('restaurantlist', {
+//     title,
+//     haltes
+//   });
 
-})
+// })
 
 // app.get('/harriet', (req, res) => {
 //   res.send('Hello Harriet!!')  
@@ -168,24 +168,15 @@ app.set('view engine', 'ejs');
 
 
 app.post("/get-locations", (req, res) => {
-
+  console.log('post request received')
   // Data uit form halen
+  const travelLocation = req.body.location;
+  const formOfTransport = req.body.transportType;
 
-  const formOfTransport = req.query.modaliteit;
-
-  fetch('https://maps.amsterdam.nl/open_geodata/geojson_lnglat.php?KAARTLAAG=TRAMMETRO_PUNTEN_2021&THEMA=trammetro')
-    .then(res => res.json())
-    .then(data => {
-
-    })
-    .catch(err => console.log(err));
+  getData() // get data from API, and we still have to pass the data to the matchData and eventually renderResults function
 
 
-
-
-
-
-})
+});
 
 
 // Data gebruiken om fetch te doen 
@@ -210,6 +201,88 @@ app.use((req, res) => {
 });
 
 
+function getData() {
+  console.log("getData function wordt nu uitgevoerd")
+  fetch('https://maps.amsterdam.nl/open_geodata/geojson_lnglat.php?KAARTLAAG=TRAMMETRO_PUNTEN_2021&THEMA=trammetro') // fetch data from API
+    .then(res => res.json()) // Convert data to json
+    .then(jsonData => cleanData(jsonData)) // clean the data
+    .then(cleanedData => matchData(cleanedData)) // matches the data from the api with the data from form input
+    .then(finalData => {
+      console.log(finalData)
+      renderResults(finalData) // render the data
+      return finalData
+    })
+    .catch(err => console.log(err)) // catch errors if the two functions above fail
+};
+
+function cleanData(data) {
+  // console.log('type van je hele dataset: ' + data.type)
+  // console.log('name van je hele dataset: ' + data.name)
+
+  // console.log('id van het eerste object: ' + data.features[0].id)
+  // console.log('geometry van het eerste object: ' + data.features[0].geometry)
+  // console.log('coordinates van het eerste object: ' + data.features[0].geometry.coordinates) // coordinaten van het object
+
+  // console.log('naam van het eerste object: ' + data.features[0].properties.Naam) // naam van de locatie
+  // console.log('modaliteit van het eerste object: ' + data.features[0].properties.Modaliteit) // vorm van transport
+
+  const alleTransports = data.features
+  const transportArray = alleTransports.map(function (transportItem) {
+    // console.log(transportItem)  // one item of the array is called transportItem. So: transportItem.geometry.coordinates, would give [4.853117, 52.358384]
+    const newTransportItem = {
+      id: transportItem.id,
+      naam: transportItem.properties.Naam,
+      modaliteit: transportItem.properties.Modaliteit,
+      coordinates: transportItem.geometry.coordinates,
+      label: transportItem.properties.Label,
+      lijn: transportItem.properties.Lijn
+    }
+    return newTransportItem
+  })
+
+  return transportArray
+}
+
+function matchData(data) {
+  console.log('matchData function wordt nu uitgevoerd')
+  console.log(data)
+  const submitForm = document.querySelector('ptForm'); //get the input data from the form on the page
+  const bus = document.querySelector('bus');
+
+  ptForm.addEventListener('submit', (e) => {
+    e.preventDefault(); //prevent the browser from going to another page once the form is submitted
+
+    if (document.querySelector('bus').checked = data.transportItem.properties.Modaliteit("Bus")) {
+      console.log("We hebben een bus!")
+    }
+
+
+
+
+
+    matchData();
+  });
+
+
+
+
+
+
+
+  // match the location & transport form with the data from the api and return matched results
+};
+
+
+function renderResults(element, data) {
+  console.log('renderResults function wordt nu uitgevoerd')
+  document.querySelector(element).innerHTML = data;
+  return data = `
+  <h2>Your best options are: ${transportItem.properties.Naam}</h2>
+ <p>this is a ${transportItem.properties.Modaliteit}</p>`
+
+
+};
+
 
 async function connectDB() {
 
@@ -230,9 +303,6 @@ async function connectDB() {
 
   }
 }
-
-
-
 
 app.listen(port, () => {
   console.log(`Web server listening on http://localhost:${port}`)
