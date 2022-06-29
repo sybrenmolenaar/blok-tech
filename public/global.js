42399c3d56dab2a2a5ad2e745cfb3d439561ae35
Sybren Molenaar <55452337+sybrenmolenaar@users.noreply.github.com>
2022-06-30 00:31:27 +0200

    added like and dislike buttons

--- public/global.js
+++ public/global.js
@@ -1,9 +1,58 @@
 //source: https://github.com/w3c/geolocation-api
-navigator.geolocation.getCurrentPosition(position => {
+
+
+
+console.log(haversineDistance);
+
+function getDataHome() {
+  return fetch('https://maps.amsterdam.nl/open_geodata/geojson_lnglat.php?KAARTLAAG=TRAMMETRO_PUNTEN_2021&THEMA=trammetro')
+    .then(res => res.json())
+    .then(data => cleanData(data))
+}
+
+function cleanData(data) {
+
+
+  const alleTransports = data.features
+  const transportArray = alleTransports.map(function (transportItem) {
+    // console.log(transportItem)  // one item of the array is called transportItem. So: transportItem.geometry.coordinates, would give [4.853117, 52.358384]
+    const newTransportItem = {
+      id: transportItem.id,
+      naam: transportItem.properties.Naam,
+      modaliteit: transportItem.properties.Modaliteit,
+      coordinates: transportItem.geometry.coordinates,
+      label: transportItem.properties.Label,
+      lijn: transportItem.properties.Lijn
+    }
+    return newTransportItem
+  })
+
+  return transportArray
+}
+
+
+navigator.geolocation.getCurrentPosition(async (position) => {
   const {
     latitude,
     longitude
   } = position.coords;
+  const data = await getDataHome();
+
+  data.forEach((item, index) => {
+    const locations = document.querySelectorAll(".stops")
+    const distance = document.createElement("div")
+    const haversine = haversineDistance(position.coords, item.coordinates) / 1000
+    const haversineRounded = Math.round(haversine)
+
+    const distanceString = `Distance ${haversineRounded}`
+
+    console.log(distanceString)
+    const distanceContent = document.createTextNode(distanceString + " KM") //voeg de boodschap "afstand tot jou" toe.
+
+    distance.appendChild(distanceContent)
+    locations[index].appendChild(distance)
+  })
+  console.log(data)
   // Do something cool with latitude, longitude
   console.log(position.coords)
 });
@@ -24,18 +73,3 @@ navigator.geolocation.getCurrentPosition(position => {
 
 // match the location & transport form with the data from the api and return matched results
 // };
-
-
-function renderResults(element, data) {
-  console.log('renderResults function wordt nu uitgevoerd')
-  const ptForm = document.querySelector('ptForm')
-
-  return data.map(data => { // loop through the data
-    gallery.innerHTML += // add the following HTML to the gallery element
-      document.querySelector(element).innerHTML = data;
-    return data = `
-  <h2>Your best options are: ${transportItem.properties.Naam}</h2>
- <p>this is a ${transportItem.properties.Modaliteit}</p>`
-
-  });
-}
--- server.js
+++ server.js
@@ -15,70 +15,7 @@ dotenv.config();
 
 let db = null;
 console.log(process.env.TESTVAR);
-const haltes = [{
-    "id": 1,
-    "slug": "Instock",
-    "name": "Instock",
-    "facilities": ["wheelchair friendly stairs", "lift"],
-    "Reviews": "Good service, lovely people. Five stars",
-  },
-  {
-    "id": 2,
-    "slug": "waterkant",
-    "name": "Waterkant",
-    "facilities": ["wheelchair friendly stairs", "lift"],
-    "Reviews": "Good service, lovely people. Five stars",
-  },
-  {
-    "id": 3,
-    "slug": "mrandmrswatson",
-    "name": "Mr & Mrs Watson",
-    "facilities": ["wheelchair friendly stairs", "lift"],
-    "Reviews": "Good service, lovely people. Five stars",
-  },
-  {
-    "id": 4,
-    "slug": "volkshotel",
-    "name": "Volkhotel",
-    "facilities": ["wheelchair friendly stairs", "lift"],
-    "Reviews": "Good service, lovely people. Five stars",
-  },
-  {
-    "id": 5,
-    "slug": "tapastheater",
-    "name": "Tapas Theater",
-    "facilities": ["wheelchair friendly stairs", "lift"],
-    "Reviews": "Good service, lovely people. Five stars",
-  },
-  {
-    "id": 6,
-    "slug": "tolhuistuin",
-    "name": "Tolhuistuin",
-    "facilities": ["wheelchair friendly stairs", "lift"],
-    "Reviews": "Good service, lovely people. Five stars",
-  },
-  {
-    "id": 7,
-    "slug": "loetje",
-    "name": "Loetje",
-    "facilities": ["wheelchair friendly stairs", "lift"],
-    "Reviews": "Good service, lovely people. Five stars",
-  },
-  {
-    "id": 8,
-    "slug": "tolhuistuin",
-    "name": "Tolhuistuin",
-    "facilities": ["wheelchair friendly stairs", "lift"],
-    "Reviews": "Good service, lovely people. Five stars",
-  },
-  {
-    "id": 3,
-    "slug": "febo",
-    "name": "FEBO",
-    "facilities": ["wheelchair friendly stairs", "lift"],
-    "Reviews": "Good service, lovely people. Five stars",
-  },
-];
+
 
 
 app.use(express.static('public'))
@@ -98,6 +35,7 @@ app.get('/results', async (req, res) => {
 
 
 
+
   // RENDER PAGE
   const title = (haltes.length == 0) ? "No stops were found" : "Stops";
   res.render('restaurantlist', {
@@ -106,28 +44,60 @@ app.get('/results', async (req, res) => {
   });
 })
 
+app.post("/likestop", async (req, res) => {
+  // haal stopId op uit button value
+  const stopId = req.body.id
+  //maak like aan in de database
+  await db.collection('matches').insertOne({
+    id: stopId
+  })
+  //re-direct naar de homepage
+  res.redirect("/")
+
+})
+
+app.post("/dislikestop", async (req, res) => {
+  // haal stopId op uit button value
+  const stopId = req.body.id
+  // verwijder like met stopId meegekregen van button 
+  await db.collection('matches').deleteOne({
+    id: stopId
+  })
+  //re-direct naar de homepage
+  res.redirect("/")
+
+})
 app.get('/', async (req, res) => {
   // RENDER PAGE
   const title = "Find the best nearest stop for you!";
 
   const type = req.query.type;
+  // haal likes op uit de database (genaamd matches). Maak hier array van. 
+  const likes = await db.collection('matches').find({}, {}).toArray()
 
-  // if (type) {
+  // like id's worden geconverteerd naar nummers zodat je ze overeenkomen met de id's uit de api. 
+  const mappedLikes = likes.map(like => Number(like.id))
 
-  console.log('post request received')
+
+  console.log(mappedLikes)
   // Data uit form halen
   // const travelLocation = req.body.location;
   // const formOfTransport = req.body.transportType;
   // console.log('formOfTransport', formOfTransport)
-  const result = await getData(type) // get data from API, and we still have to pass the data to the matchData and eventually renderResults function
+  const result = await getDataHome() // get data from API, and we still have to pass the data to the matchData and eventually renderResults function
 
   console.log(result.length)
 
   // res.send(result);
   res.render('index', {
     stops: result,
-    title: 'success'
+    title: 'success',
+    likes: mappedLikes,
+
   })
+
+
+
 
 
   // return
@@ -141,50 +111,18 @@ app.get('/', async (req, res) => {
 
 });
 
-app.get('/restaurants/:id/:slug', (req, res) => {
-  // FIND RESTAURANT
-  const id = req.params.id
-  const restaurant = haltes.find(element => element.id == id)
-  console.log(restaurant)
-  // RENDER PAGE
-  res.render('restaurantdetails', {
-    title: `details for restaurant ${restaurant.name}`,
-    restaurant
-  });
-});
+app.post("/filter", async (req, res) => {
+  console.log("form data", req.body);
 
-app.get('/restaurant/add', (req, res) => {
-  // RENDER PAGE
-  const title = "add a new restaurant";
-  res.render('addrestaurant', {
-    title
-  });
-});
 
-// app.post('/ditistijdelijk', (req, res) => {
+  const data = await getData(req.body.type);
+  //get the data from the API, wait for the response of the API before you send the content.
+  console.log(data);
 
-//   const restaurant = {
-//     name: req.body.name
-//   }
-//   halte.push(halte);
-//   console.log("this", haltes);
-//   title = "Het is gelukt!";
-//   res.render('restaurantlist', {
-//     title,
-//     haltes
-//   });
-
-// })
-
-// app.get('/harriet', (req, res) => {
-//   res.send('Hello Harriet!!')  
-// })  
-// app.get('/sybren', (req, res) => {
-//   res.send('Hoi Sybren!!')  
-// })  
-// app.get('/name/:name', (req, res) => {
-//   res.send(`Hello ${req.params.name}`)
-// })  
+  res.render('index', { //render the data on the page, show the stops and the title.
+    stops: data,
+  })
+})
 
 app.use(express.static('public'))
 // app.use(bodyParser.json());
@@ -192,30 +130,6 @@ app.use(express.static('public'))
 
 app.set('view engine', 'ejs');
 
-
-
-
-
-
-
-
-
-// Data gebruiken om fetch te doen 
-// Data gebruiken om fetch te doen 
-// Data gebruiken om fetch te doen 
-// fetch('https://maps.amsterdam.nl/open_geodata/geojson_lnglat.php?KAARTLAAG=TRAMMETRO_PUNTEN_2021&THEMA=trammetro').then(data => {
-//   console.log();
-//   return data.json();
-// }).then(locations => {
-
-//   locations.
-//   res.render('index', {
-//     locaties: locations
-//   })
-// })
-//   // Pagina opnieuw weergeven met data
-
-// })
 
 app.use((req, res) => {
   res.status(404).send('Error 404: file not found')
@@ -230,6 +144,12 @@ function getData(formOfTransport) {
     .then(cleanedData => matchData(cleanedData, formOfTransport)) // matches the data from the api with the data from form input
     .catch(err => console.log(err)) // catch errors if the two functions above fail
 };
+
+function getDataHome() {
+  return fetch('https://maps.amsterdam.nl/open_geodata/geojson_lnglat.php?KAARTLAAG=TRAMMETRO_PUNTEN_2021&THEMA=trammetro')
+    .then(res => res.json())
+    .then(data => cleanData(data))
+}
 
 function cleanData(data) {
   // console.log('type van je hele dataset: ' + data.type)
@@ -268,6 +188,7 @@ function matchData(data, formOfTransport) {
 
   return data.filter((item) => item.modaliteit.toLowerCase() === formOfTransport)
 };
+
 
 
 // function renderResults(element, data) {
